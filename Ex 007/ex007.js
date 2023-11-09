b0aaf85fbe7dd03af63c3a6f3285da1cdd49d10b
Lorrane Souza <soulorrane00@gmail.com>
2023-11-09 18:31:10 -0300

    Condições em JS

---
+++ Ex 007/ex007.js
@@ -0,0 +1,9 @@
+var vel = 65
+console.log(`A velocidade do seu carro é de aproximadamente ${vel}km/h`)
+
+if(vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade permitido. MULTADO!`)
+}
+
+console.log(`Dirija sempre utilizando o cinto de segurança!`)
+
