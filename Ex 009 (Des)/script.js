7367b4ada205a726cb1748b085549693d6e9d13f
Lorrane Souza <soulorrane00@gmail.com>
2023-11-14 17:29:33 -0300

    novo exercício

--- Ex 009 (Des)/script.js
+++ Ex 009 (Des)/script.js
@@ -3,15 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+   //var hora = 19
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora <  12) {
-        // BOM DIA!
-        img.src = 'manha.jpg'
-    } else if (hora >= 12 && hora < 18) {
         // BOA TARDE!
-        img.src = 'tarde.jpg'
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#A66F3F'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#F2784B'
     } else {
         //BOA NOITE!
-        img.src = 'noite.jpg'
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#023373'
     }
 }
