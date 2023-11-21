5c21503492142b72b5fc17abdb72b341ea8fe408
Lorrane Souza <soulorrane00@gmail.com>
2023-11-21 18:03:59 -0300

    conceitos JS

--- Ex 011/teste.js
+++ Ex 011/teste.js
@@ -1,9 +1,3 @@
-var c = 1
-do {
-    console.log(`passo ${c}`)
-    c++
-} while (c <= 6)
-
 /*
 var c = 1
 while (c <= 6) {
@@ -20,3 +14,22 @@ while (c <= 6) {
     console.log('Tudo bem?')
     console.log('Tudo bem?')
 */
+
+/*
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6) */
+
+/*
+for (var c = 1 ; c <= 5 ; c++) {
+    console.log(c)
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1 ; c<= 5 ; c++) {
+    console.log(c)
+}
+console.log('FIM!')
